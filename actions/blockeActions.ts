aa0c834d3bc55dec2c9d9c6e427c14dca8ef41ff
Xukooru <xukoorudev@gmail.com>
2024-01-03 09:03:42 +0500

    Deploye4

--- actions/blockeActions.ts
+++ actions/blockeActions.ts
@@ -10,59 +10,34 @@ const roomService = new RoomServiceClient(
    process.env.LIVEKIT_API_URL!,
    process.env.LIVEKIT_API_KEY!,
    process.env.LIVEKIT_API_SECRET!,
-)
-
-
-export const onBlock = async (id: string) => {
-   const self = await getSelf()
-
+ );
+ 
+ export const onBlock = async (id: string) => {
+   const self = await getSelf();
+ 
    let blockedUser;
-
+ 
    try {
-      blockedUser = await blockUser(id)
-
+     blockedUser = await blockUser(id);
    } catch {
-      // This means user is guest
-      // throw new Error("Internal Error")
+     // This means user is a guest
    }
-
+ 
    try {
-      await roomService.removeParticipant(self.id, id)
+     await roomService.removeParticipant(self.id, id);
    } catch {
-      // this means user is not in the room
+     // This means user is not in the room
    }
-
-   revalidatePath(`/u/${self.username}/community`)
-
-   return blockedUser
-}
-
-// export const onUnblock =async (id:string) => {
-//       const self = await getSelf()
-//       const unblockedUser = await unblockUser(id)
-
-//       revalidatePath('/')
-
-     
-//       revalidatePath(`/u/${self.username}/community`)
-      
-      
-//       return unblockedUser
-
-// }
-export const onUnblock =async (id:string) => {
-   try {
-      const unblockedUser = await unblockUser(id)
-
-      revalidatePath('/')
-
-      if (unblockedUser) {
-         revalidatePath(`/${unblockedUser.blocked.username}`)
-      }
-      
-      return unblockedUser
-
-   } catch (error) {
-      throw new Error("Internal Error")
-   }
-}
+ 
+   revalidatePath(`/u/${self.username}/community`);
+ 
+   return blockedUser;
+ };
+ 
+ export const onUnblock = async (id: string) => {
+   const self = await getSelf();
+   const unblockedUser = await unblockUser(id);
+ 
+   revalidatePath(`/u/${self.username}/community`);
+   return unblockedUser;
+ };
